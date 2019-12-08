1a3b9c170a8367bee1034df4d3aab31458d725e9
Michael Loughry <miclo@microsoft.com>
2019-12-08 10:45:35 -0800

    Day 3, part 1

---
+++ src/day03/index.ts
@@ -0,0 +1,97 @@
+import { Solution } from "../common/SolutionFile";
+
+export default function solve(input: string): Solution {
+  const wires = input
+    .split("\n")
+    .map(wire => wire.split(","))
+    .map(parseWireInstructions);
+
+  const part1 = [...wires[0].intersect(wires[1])].reduce(
+    (minDistance, intersection) =>
+      Math.min(minDistance, Math.abs(intersection[0]) + Math.abs(intersection[1])),
+    Number.MAX_SAFE_INTEGER
+  );
+  return {part1};
+}
+
+function parseWireInstructions(wireInstructions: string[]): CoordinateSet {
+  const wireCoords = new CoordinateSet();
+
+  let currentPosition: [number, number] = [0, 0];
+  const instructionRegex = /^(?<dir>U|D|R|L)(?<distance>\d+)$/;
+
+  for (const instruction of wireInstructions) {
+    const { groups } = instructionRegex.exec(instruction)!;
+    const distance = parseInt(groups!.distance);
+    let newPosition: [number, number];
+    let [x, y] = currentPosition;
+
+    switch (groups!.dir) {
+      case "U":
+        newPosition = [x, y + distance];
+        break;
+      case "D":
+        newPosition = [x, y - distance];
+        break;
+      case "L":
+        newPosition = [x - distance, y];
+        break;
+      case "R":
+        newPosition = [x + distance, y];
+        break;
+      default:
+        throw new Error("Invalid direction");
+    }
+
+    wireCoords.addRange(currentPosition, newPosition);
+    currentPosition = newPosition;
+  }
+
+  wireCoords.delete(0, 0);
+
+  return wireCoords;
+}
+
+class CoordinateSet {
+  private coords: Map<number, Set<number>> = new Map<number, Set<number>>();
+
+  add(x: number, y: number) {
+    const row = this.coords.get(x) ?? new Set<number>();
+    row.add(y);
+    this.coords.set(x, row);
+  }
+
+  addRange([ax, ay]: [number, number], [bx, by]: [number, number]) {
+    for (let x = Math.min(ax, bx); x <= Math.max(ax, bx); x++) {
+      for (let y = Math.min(ay, by); y <= Math.max(ay, by); y++) {
+        this.add(x, y);
+      }
+    }
+  }
+
+  delete(x: number, y: number) {
+    this.coords.get(x)?.delete?.(y);
+  }
+
+  *intersect(other: CoordinateSet): IterableIterator<[number, number]> {
+    for (const [x, row] of this.coords) {
+      if (other.coords.has(x)) {
+        for (const y of row) {
+          if (other.coords.get(x)?.has(y)) {
+            yield [x, y];
+          }
+        }
+      }
+    }
+    return;
+  }
+
+  *[Symbol.iterator](): IterableIterator<[number, number]> {
+    for (const [x, row] of this.coords) {
+      for (const y of row) {
+        yield [x, y];
+      }
+    }
+    return;
+  }
+}
